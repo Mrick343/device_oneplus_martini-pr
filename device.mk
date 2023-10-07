fc4fbcf9e19fb3ec0e54088736ef74960e0873eb
Mrick343 <mrinmoy8429de@gmail.com>
2023-10-07 15:58:02 +0530

    martini: Remove packages at build time Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- device.mk
+++ device.mk
@@ -61,6 +61,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    remove
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
