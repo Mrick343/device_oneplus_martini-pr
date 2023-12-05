05e0988cfb28036b7fa913217dede280a521e3da
Sugakesshaa <sugakesshaa@gmail.com>
2023-12-05 12:26:45 +0800

    martini: Update to OxygenOS 14 MT2111_14.0.0.80(EX01)
    - Pull in oplus_camera_config in tree to make further changes
    
    Change-Id: Id38cb6804c5fab593bceadf7244c92c4008eb5a3

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/camera/oplus_camera_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_config
+
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/display/display_id_4630946728207028354.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946728207028354.xml
